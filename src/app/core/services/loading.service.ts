73177e2ab64c7c4764f4761f75790ead5f239f47
mahdi <mahdiGhorbaniMQ@gmail.com>
2022-08-06 21:16:25 +0430

    loading module updated

--- src/app/core/services/loading.service.ts
+++ src/app/core/services/loading.service.ts
@@ -7,8 +7,13 @@ import { BehaviorSubject, Subject } from 'rxjs';
 export class LoadingService {
 
   constructor() {
+    setTimeout(() => {
+      this.isLoading = false
+    }, 1500);
     this.loaded.subscribe(data=>{
       this.loadedData.push(data)
+      console.log(this.loadedData);
+      
       if(data.length===3){
         this.isLoading = false
       }
